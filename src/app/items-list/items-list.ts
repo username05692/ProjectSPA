2485e4254cba1c9bb62d91af40f480d4b1c74b6a
username05692 <elnikovaanna3@gmail.com>
2025-11-29 20:52:02 +0200

    Заповнення тіла сторінки

---
+++ src/app/items-list/items-list.ts
@@ -0,0 +1,56 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Track } from '../shared/models/track';
+import { ItemCard } from '../item-card/item-card';
+
+@Component({
+  selector: 'app-items-list',
+  standalone: true,
+  imports: [CommonModule, ItemCard],
+  templateUrl: './items-list.html',
+  styleUrl: './items-list.css',
+})
+export class ItemsList {
+
+
+  tracks: Track[] = [
+    {
+      id: 1,
+      title: 'Where The Hell Is My Husband',
+      artist: 'RAYE',
+      genre: 'Alternative R&B',
+      duration: 158,
+      releaseYear: 2025,
+      albumArtUrl: 'assets/ray.png'
+    },
+    {
+      id: 2,
+      title: 'SKINNY',
+      artist: 'Billie Eilish',
+      genre: 'Pop/Alternative',
+      duration: 125,
+      releaseYear: 2024,
+      albumArtUrl: 'assets/skiny.png'
+    },
+    {
+      id: 3,
+      title: 'boy',
+      artist: 'Кажанна',
+      genre: 'Pop/Electronic',
+      duration: 174,
+      releaseYear: 2023,
+      albumArtUrl: 'assets/boy1.png'
+    },
+    {
+      id: 4,
+      title: 'Eyes Closed',
+      artist: 'Imagine Dragons',
+      genre: 'Alternative Rock',
+      duration: 200,
+      releaseYear: 2024,
+      albumArtUrl: 'assets/eyes.png'
+    },
+  ];
+
+}
